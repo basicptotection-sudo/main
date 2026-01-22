6ed596dc8b5a97a38b7c8f1c8d0f6adf92dd69cb
Basic Protection <basic.ptotection@gmail.com>
2026-01-22 14:48:27 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-5082922214-6cb07",
+  "appId": "1:893247255936:web:c331c28e0d5eaffe4b3d65",
+  "apiKey": "AIzaSyA5l0L_FiDeYoWDIbVchmULLAsHmqRT77M",
+  "authDomain": "studio-5082922214-6cb07.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "893247255936"
+};
