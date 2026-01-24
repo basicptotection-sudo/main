eda3c7bb162cdb625f54974836a2f407f834db15
Basic Protection <basic.ptotection@gmail.com>
2026-01-24 11:36:44 +0000

    corrige [{ 	"resource": "/home/user/studio/src/app/globals.css", 	"owner

--- tailwind.config.js
+++
@@ -1,6 +0,0 @@
-/**
- * @file This file is redundant and can be safely deleted.
- * The project is configured to use tailwind.config.ts for Tailwind CSS configuration.
- * This file has been emptied to prevent any potential build conflicts or confusion.
- */
-module.exports = {};
