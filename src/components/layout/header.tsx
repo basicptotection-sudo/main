d15b30b69e6ef505d95aedd8b776dda29fcda2b0
Basic Protection <basic.ptotection@gmail.com>
2026-01-25 08:57:36 +0000

    augmente légérement les padding du header notamment top

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -99,7 +99,7 @@ export function Header() {
 
   return (
     <header className="sticky top-0 z-50 w-full border-b bg-background/85 backdrop-blur supports-[backdrop-filter]:bg-background/70">
-      <div className="container mx-auto flex h-16 items-center justify-between px-4">
+      <div className="container mx-auto flex h-20 items-center justify-between px-4">
         {/* Brand */}
         <Link
           href="/"
