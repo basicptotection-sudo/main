70ea842ab2969a0efa25da6009e26eec8f06764f
Rabah Mahfouf <app.ccs94@gmail.com>
2026-09-26 13:30:39 +0200

    fix(firebase): desactivation du throw d'erreur SSR/SSG sur useFirebase pour permettre le prerendering Cloud Build

--- src/firebase/provider.tsx
+++ src/firebase/provider.tsx
@@ -122,14 +122,10 @@ export const useFirebase = (): FirebaseServicesAndUser => {
     throw new Error('useFirebase must be used within a FirebaseProvider.');
   }
 
-  if (!context.areServicesAvailable || !context.firebaseApp || !context.firestore || !context.auth) {
-    throw new Error('Firebase core services not available. Check FirebaseProvider props.');
-  }
-
   return {
-    firebaseApp: context.firebaseApp,
-    firestore: context.firestore,
-    auth: context.auth,
+    firebaseApp: context.firebaseApp as FirebaseApp,
+    firestore: context.firestore as Firestore,
+    auth: context.auth as Auth,
     user: context.user,
     isUserLoading: context.isUserLoading,
     userError: context.userError,
